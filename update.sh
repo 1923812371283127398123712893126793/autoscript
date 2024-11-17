94497abf23e8677235963cfe240a645cb3a0d4b9
1923812371283127398123712893126793 <104192289+1923812371283127398123712893126793@users.noreply.github.com>
2024-11-17 22:02:55 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/emmkash2070/autoscript/main/ubuntu/menu.zip
+    wget https://github.com/1923812371283127398123712893126793/autoscript/raw/refs/heads/main/ubuntu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
